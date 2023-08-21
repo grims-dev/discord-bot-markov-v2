fadca2ad3defff7887051a1ffd6cfc8268f9e8b7
grims-dev <ciarangrimshawdev@gmail.com>
2023-08-21 19:30:27 +0100

    discord code block regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,4 @@
 export const matchPunctuation = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
 export const matchAlphanumeric = /[A-z 0-9]/g;
 export const matchWhitespace = /\s+/g;
+export const matchDiscordCodeBlock = /(```)(.*?)(```)/gims;
